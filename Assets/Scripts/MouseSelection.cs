d927c8688dec84f216cc60f47edc94f22b41a6d7
makled <elhassan.makled@gmail.com>
2020-10-20 16:11:56 +0200

    feat(script): create mouse object selection

---
+++ Assets/Scripts/MouseSelection.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mkld.Photoshoot
+{
+    public class MouseSelection : MonoBehaviour
+    {
+        private GameObject selectedObject;
+
+        [Tooltip("This material will be used to identify selected object")]
+        public Material SelectionMaterial;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SelectObject(Input.mousePosition);
+            }
+        }
+
+        public GameObject GetSelectedObject()
+        {
+            return selectedObject;
+        }
+
+        void SelectObject(Vector3 mousePosition)
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(mousePosition), out hitInfo);
+
+            if (hit)
+            {
+                RemoveSelection();
+
+                selectedObject = hitInfo.transform.gameObject;
+
+                GameObject obj = Instantiate(selectedObject, this.transform);
+                obj.transform.position = selectedObject.transform.position;
+                obj.transform.rotation = selectedObject.transform.rotation;
+
+                SetSelectionMaterial(obj.GetComponent<Renderer>());
+            }
+            else
+            {
+                RemoveSelection();
+            }
+        }
+
+
+        void RemoveSelection()
+        {
+            selectedObject = null;
+            foreach (var child in GetComponentsInChildren<Transform>())
+            {
+                if (child.gameObject.CompareTag("SelectionManager"))
+                {
+                    continue;
+                }
+                else
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+        }
+
+        void SetSelectionMaterial(Renderer renderer)
+        {
+            for (int i = 0; i < renderer.materials.Length; i++)
+            {
+                renderer.materials[i] = SelectionMaterial;
+            }
+        }
+    }
+}
