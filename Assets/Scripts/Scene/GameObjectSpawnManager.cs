79d321f44e7a8075f6a4f88a3b723f53c40a2b68
makled <elhassan.makled@gmail.com>
2020-10-21 15:42:20 +0200

    feat(script): create scene manager script
    the purpose of this script is to create and add new objects to the scene

---
+++ Assets/Scripts/Scene/GameObjectSpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mkld.Photoshoot
+{
+    public class GameObjectSpawnManager : MonoBehaviour
+    {
+        private List<GameObject> SpawnedObjects;
+        public static GameObjectSpawnManager Instance;
+        // Start is called before the first frame update
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        void Start()
+        {
+            SpawnedObjects = new List<GameObject>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void AddObject(GameObject obj)
+        {
+            GameObject spawnedObj = Instantiate(obj);
+            SpawnedObjects.Add(spawnedObj);
+        }
+
+    }
+}
+
