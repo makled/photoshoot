82b4df616e58f002ffed4cba0de720bb0a87cb2f
makled <elhassan.makled@gmail.com>
2020-10-21 15:39:30 +0200

    feat(script): create scriptable object for key objects that could be added by the user

---
+++ Assets/Scripts/Objects/SpawnableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mkld.Photoshoot
+{
+    [CreateAssetMenu(fileName="New Spawnable Object", menuName = "PhotoShoot/Spawnable Object", order = 1)]
+    public class SpawnableObject : ScriptableObject
+    {
+        [Tooltip("The name of the object that will be displayed in the UI")]
+        public string Name;
+
+        [Tooltip("Description of the object")]
+        [TextArea]
+        public string Description;
+
+        [Tooltip("Prefab reference of the object itself")]
+        public GameObject ObjectPrefabAssetReference;
+        [Tooltip("Image that will be displayed in the UI")]
+        public Sprite UIImage;
+    }
+}
+
---
+++ Assets/Scripts/Objects/SpawnableObjectList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mkld.Photoshoot
+{
+    [CreateAssetMenu(fileName="New Spawnable Objects List", menuName = "PhotoShoot/Spawnable Objects List", order = 1)]
+    public class SpawnableObjectList : ScriptableObject
+    {
+        [Tooltip("List of all possible Spawnable Objects")]
+        public List<SpawnableObject> ListOfSpawnableObjects;
+    }
+}
+
