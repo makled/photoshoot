e7e35a0b34e5112631e78fac91aff84c371ae718
makled <elhassan.makled@gmail.com>
2020-10-21 15:37:48 +0200

    feat(UI): add user interface to add new objects to scene

---
+++ Assets/Scripts/UI/ObjectListController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mkld.Photoshoot
+{
+    public class ObjectListController : MonoBehaviour
+    {
+        [Tooltip("Prefab for UI Object Element")]
+        public GameObject UIElementPrefab;
+
+        [Tooltip("Reference to the list of the spawnable object")]
+        public SpawnableObjectList SpawnableObjectListReference;
+        // Start is called before the first frame update
+        void Start()
+        {
+            PopulateListOfObjects();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void PopulateListOfObjects()
+        {
+            foreach(var obj in SpawnableObjectListReference.ListOfSpawnableObjects)
+            {
+                GameObject elemObj = Instantiate(UIElementPrefab, this.transform);
+                elemObj.GetComponent<ObjectListElementController>().SetSpawnableObjectReference(obj);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/UI/ObjectListElementController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace mkld.Photoshoot
+{
+    public class ObjectListElementController : MonoBehaviour
+    {
+        [Tooltip("Text Object reference")]
+        public TextMeshProUGUI TextObjectReference;
+
+        [Tooltip("Image Object Reference")]
+        public Image imageObject;
+
+        [Tooltip("Reference To Spawnable Object")]
+        public SpawnableObject ObjectReference;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetSpawnableObjectReference(SpawnableObject referenceObj)
+        {
+            ObjectReference = referenceObj;
+            imageObject.sprite = ObjectReference.UIImage;
+            TextObjectReference.text = ObjectReference.name;
+        }
+
+        public void OnElementClick()
+        {
+            if (ObjectReference == null)
+                return;
+                
+            GameObjectSpawnManager.Instance.AddObject(ObjectReference.ObjectPrefabAssetReference);
+            
+        }
+    }
+}
+
